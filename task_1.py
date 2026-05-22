e728ce4764c831958721706f543ae09b5d08cf31
Axeme01 <daniil_balandin01@mail.ru>
2026-05-22 08:51:58 +0700

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -3,8 +3,8 @@ import os
 import math
 
 def calcular(x,y):
-    result=0
+    result = 0
     for i in range(0,10):
-        if i%2==0:
+        if i%2 == 0:
             result +=math.pow(x,i)/math.factorial(i)
     return result+y
